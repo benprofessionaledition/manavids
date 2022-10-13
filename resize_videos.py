f4f865e83489dbb9e88419c77ce80cc90b87db73
blevine <blevine@linkedin.com>
2022-10-12 18:29:38 -0700

    base logic works just need to 'productionize'

--- resize_videos.py
+++ resize_videos.py
@@ -11,8 +11,9 @@ logging.basicConfig(level=logging.DEBUG)
 # function to find the resolution of the input video file
 VideoInfo = namedtuple('VideoInfo', ['height', 'width', 'duration_seconds'])
 
-SCALING_DEFAULT=10.52
-SCALING_CATS=14.43
+SCALING_DEFAULT = 10.52
+SCALING_CATS = 14.43
+
 
 def probe_video(filename: str) -> VideoInfo:
     cmd = "ffprobe -v quiet -print_format json -show_streams"
@@ -29,6 +30,7 @@ def probe_video(filename: str) -> VideoInfo:
     duration = vidstream['duration']
     return VideoInfo(int(height), int(width), float(duration))
 
+
 def crop_to_square(filepath, scaling_constant=SCALING_DEFAULT):
     # crop to 1:1, change speed, clip at 3 and 6 seconds, paste reversed, export at 480x480
     info = probe_video(filepath)
@@ -40,10 +42,27 @@ def crop_to_square(filepath, scaling_constant=SCALING_DEFAULT):
     stream = (
         ffmpeg
         .input(filepath)
-        .filter('crop', 'in_h', 'in_h') # crop to 1:1
-        .filter('setpts', f'PTS*{scaling_factor}') # make them play for exactly the same length, since the exported videos will be slightly different
-        .filter('trim', start=3, end=6) # clip before 3 seconds, after 6 seconds
-        # .filter('fps', fps=30, round='up')
+        .filter('crop', 'in_h', 'in_h')  # crop to 1:1
+        # make them play for exactly the same length, since the exported videos will be slightly different
+        .filter('setpts', f'PTS*{scaling_factor}')
+        # clip before 3 seconds, after 6 seconds
+        .filter('trim', start=3, end=6)
+        # reset pts so the whole thing is 3 seconds
+        .filter('setpts', 'PTS-STARTPTS')
+        # 30 fps, 480x480
+        .filter('fps', fps=30, round='up')
+        .filter('scale', '480', '480')
+        .split()
+    )
+
+    stream_rev = (
+        stream[1]
+        .filter('reverse')
+        .filter('setpts', 'PTS-STARTPTS')
+    )
+
+    (
+        ffmpeg.concat(stream[0], stream_rev)
         .output("resources/cropped.mp4")
         .overwrite_output()
         .run()
