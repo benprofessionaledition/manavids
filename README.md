46fea7f9a3c51db52955c752bbf06acc5984a510
Ben Levine <ben.h.levine@gmail.com>
2023-03-05 16:23:40 -0800

    added a stitch function

---
+++ README.md
@@ -0,0 +1,3 @@
+# Manavids
+Stuff for Mana's AI art so that she'll sit with me on the couch instead of doing tedious shit with video editing
+
--- resize_videos.py
+++ resize_videos.py
@@ -1,6 +1,9 @@
+import atexit
 from collections import namedtuple
 import os
+import shutil
 import sys
+import time
 import ffmpeg
 import subprocess
 import shlex
@@ -71,28 +74,121 @@ def crop_to_square(input_file: str, output_file: str, output_res: str, scaling_c
         .run()
     )
 
-def main(args):
-    parser = argparse.ArgumentParser(description="Mana's video processing script")
-    parser.add_argument("input", help="The input directory to process")
-    parser.add_argument("--output-dir", default=None, help="The output directory. If none specified, creates a directory called \"output\" inside the input directory")
-    parser.add_argument("--time-scale", default=10.52, help="The timescale to normalize input files to")
-    parser.add_argument("--output-resolution", default="480", help="The output resolution. All output is square, so only one number is required")
-    args = parser.parse_args(args)
-    
+def _process_input_output_dir(args: argparse.Namespace):
     input_dir = args.input
     if args.output_dir is None:
         output_dir = os.path.join(input_dir, "processed_output")
     else:
         output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
+    return input_dir, output_dir
 
-    time_scale = args.time_scale
-    output_resolution = args.output_resolution
+def resize(args: argparse.Namespace):
+    input_dir, output_dir = _process_input_output_dir(args)
+    time_scale: float = args.time_scale
+    output_resolution: int = args.output_resolution
     
     for filename in tqdm(os.listdir(input_dir)):
+        if not filename.endswith(".mp4"):
+            continue
         input_file = os.path.join(input_dir, filename)
         output_file = os.path.join(output_dir, filename)
         time_scale=float(time_scale)
         crop_to_square(input_file, output_file, output_resolution, time_scale)
+
+def _ls(directory):
+    return sorted((os.path.join(directory, f)) for f in os.listdir(directory))
+
+def stitch(args: argparse.Namespace, clean: bool=True):
+    input_dir, output_dir = _process_input_output_dir(args)
+    """
+    These are the ways we can do this:
+    1. The stupid way, but it's known to work with ffmpeg: create very short ~0.1s videos of the respective
+    parts of each video, then stitch all together
+    2. The intuitive way: create a new video and append the relevant parts of every file. This makes sense 
+    but I don't think it works with videos/ffmpeg
+    3. Another possibly less stupid way: save all the frames individually, instead of videos, to the dir and 
+    then make a video out of them
+    """
+    # get all videos
+    video_filenames = sorted([os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith(".mp4")])
+
+    # make a tmp dir
+    timestr = str(int(time.time()))
+    tmp_dir = os.path.join(output_dir, f"tmp{timestr}")
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    # create "subvideos"
+    # The following would skip the first 30 seconds, and then extract the next 10 seconds to a file called output.wmv:
+    # ffmpeg -ss 30 -i input.wmv -c copy -t 10 output.wmv
+    # In the above command, the timestamps are in seconds (s.msec), but timestamps can also be in HH:MM:SS.xxx format. The following is equivalent:
+    # ffmpeg -ss 00:00:30.0 -i input.wmv -c copy -t 00:00:10.0 output.wmv
+    
+    # assume they're all the same length
+    info = probe_video(video_filenames[0])
+    total_duration = info.duration_seconds
+    
+    # we're going to make it loop over all of them
+    target_duration = args.clip_duration
+    num_vids = len(video_filenames)
+    num_clips = int(total_duration / target_duration) # round down
+    for e, i in enumerate(range(num_clips)):
+        clip_video = video_filenames[i % num_vids]
+        # round to avoid fp shit
+        clip_start = round(i * target_duration, 2)
+        clip_end = round(clip_start + target_duration, 2)
+        output_filename = os.path.join(tmp_dir, f"{e:06d}.mp4")
+        # this creates fucked up videos
+        # cmd = f"ffmpeg -ss {clip_start} -i {clip_video} -c copy -t {target_duration} {output_filename}"
+        # subprocess.run(shlex.split(cmd))
+
+        (
+            ffmpeg
+                .input(clip_video)
+                # clip before 3 seconds, after 6 seconds
+                .filter('trim', start=clip_start, end=clip_end)
+                # reset pts so the whole thing is 3 seconds
+                .filter('setpts', 'PTS-STARTPTS')
+                # save
+                .output(output_filename)
+                .overwrite_output()
+                .run()
+        )
+
+
+    # concat video files: ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp4
+    # the blessed way to concat files is to create a text file of filenames
+    filenames = [f"file '{f}'" for f in _ls(tmp_dir)]
+    txt_filename = os.path.join(tmp_dir, "files.txt")
+    with open(txt_filename, "w+") as outf:
+        outf.writelines("\n".join(filenames))
+    output_filename = os.path.join(output_dir, "output.mp4")
+    cmd = f"ffmpeg -f concat -safe 0 -i {txt_filename} -c copy {output_filename}"
+    subprocess.run(shlex.split(cmd))
+    # shutil.rmtree(tmp_dir)
+
+def main(args):
+
+    parser = argparse.ArgumentParser(description="Wifey's video processing script")
+    
+    # note to ben - you need to add subparser-specific args before global ones for some reason
+    subparsers = parser.add_subparsers()
+    sub_resize = subparsers.add_parser("resize", help="Resize a directory of videos")
+    sub_resize.add_argument("--time-scale", default=10.52, help="The timescale to normalize input files to")
+    sub_resize.add_argument("--output-resolution", default="480", help="The output resolution. All output is square, so only one number is required")
+    sub_resize.set_defaults(func=resize)
+
+    sub_stitch = subparsers.add_parser("stitch", help="Stitches all videos in a directory into one video")
+    sub_stitch.add_argument("--clip-duration", default=0.2, type=float, help="the duration for each subclip")
+    sub_stitch.set_defaults(func=stitch)
+
+    parser.add_argument("input", help="The input directory to process")
+    parser.add_argument("--output-dir", default=None, help="The output directory. If none specified, creates a directory called \"output\" inside the input directory")
+    
+    args = parser.parse_args(args)
+    args.func(args)
+    
+    
 
 
 if __name__ == '__main__':
