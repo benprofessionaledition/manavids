b867e48a17e4d763877509799cac86f44e1a8f55
Ben Levine <ben.h.levine@gmail.com>
2024-06-19 13:15:39 -0700

    updated vids2imgs.py

--- mana_lora/scripts/vids2imgs.py
+++ mana_lora/scripts/vids2imgs.py
@@ -7,13 +7,15 @@ import os
 from pathlib import Path
 import sys
 import logging
+import argparse
+
+from joblib import Parallel, delayed
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
-IMPORT_DIRECTORY = "~/mana_videos/"
-EXPORT_DIRECTORY = "~/mana_videos/processed_output/"
 
-def convert_video(file_in: str):
+def convert_video_png(file_in: str, export_directory: str):
     # base cmd: ffmpeg -skip_frame nokey -i file -vsync 0 -frame_pts true out%d.png
     logger.debug(f"Converting {file_in}")
     file_in_fname = Path(file_in).stem
@@ -22,12 +24,41 @@ def convert_video(file_in: str):
         .filter('skip_frame', 'nokey')
         .vsync(0)
         .filter('framepts', 'true')
-        .output(f'{EXPORT_DIRECTORY}/{file_in_fname}_out%d.png')
+        .output(f'{export_directory}/{file_in_fname}_out%d.png')
+        .run()
+    )
+
+def convert_video_gif(file_in: str, export_directory: str):
+    logger.debug(f"Converting {file_in}")
+    file_in_fname = Path(file_in).stem
+    (
+        ffmpeg.input(file_in)
+        .output(f'{export_directory}/{file_in_fname}.gif', vf='fps=25,scale=320:-1:flags=lanczos', format='gif')
         .run()
     )
 
 if __name__ == "__main__":
-    for file in os.listdir(IMPORT_DIRECTORY):
-        if file.endswith(".mp4"):
-            convert_video(file)
-            sys.exit(0)
+    parser = argparse.ArgumentParser(description="Converts videos to images")
+    parser.add_argument("import_directory", help="The directory where all the videos are")
+    parser.add_argument("--png", action="store_true", help="Convert videos to PNG images")
+    parser.add_argument("--gif", action="store_true", help="Convert videos to GIF images")
+    args = parser.parse_args()
+
+    import_dir = args.import_directory
+    if not os.path.exists(import_dir):
+        raise ValueError("Import directory doesn't exist")
+    output_dir = os.path.join(import_dir, "processed_videos")
+    os.makedirs(output_dir, exist_ok=True)
+
+    files = [os.path.join(import_dir, f) for f in os.listdir(import_dir) if f.endswith(".mp4")]
+    if args.png:
+        func = convert_video_png
+    elif args.gif:
+        func = convert_video_gif
+    else:
+        parser.error("Please specify either --png or --gif flag")
+
+    Parallel()(delayed(func)(filename, output_dir) for filename in files)
+
+    logger.info("Converted %d files. Their output can be found at: %s", len(files), output_dir)
+    sys.exit(0)
